e5ad7dc72eabeb52f9f62ea6f0fc6779fcec7f70
Jhonatha Cirilo Silva <jcirilo@motorola.com>
2025-07-05 22:31:11 -0400

    Add requirments.txt and changes on main.py

--- src/main.py
+++ src/main.py
@@ -6,6 +6,9 @@ from Models.CoinContainer import CoinContainer
 load_dotenv()
 
 def main(page: ft.Page):
+    page.title = "CoinRank"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER 
     coinReq = CoinRequisition()
     top5Coins:list[Coin] = coinReq.get_coins(limit=5)
 
